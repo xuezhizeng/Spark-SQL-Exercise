47d1097da72ef2549d9909565995c74c4b9a9c50
Xuanyu Wang <xuanyu.wang@outlook.com>
2017-02-17 14:57:42 -0400

    fix query

--- babyNames/babyNames.py
+++ babyNames/babyNames.py
@@ -1,6 +1,7 @@
 from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
+import time
 
 sc = SparkContext("local", "myapp")
 sq = SQLContext(sc)
@@ -19,27 +20,32 @@ childbirthDF.createOrReplaceTempView("Births")
 # Running SQL query
 # MAX year = 2014, MIN year = 1880
 year = input("The year (from 1880 to 2014): ")
+start = time.time()
 totalBirthDF = sq.sql("Select SUM(Count) from Births WHERE Year='" + str(year) + "'")
 totalBirthDF.show()
+end = time.time()
+print("query1 time: " + str(end - start))
 
+start = time.time()
 totalBirthByGenderDF = sq.sql("Select Gender, SUM(Count) from Births WHERE Year='" + str(year) + "' GROUP BY Gender")
 totalBirthByGenderDF.show()
+end = time.time()
+print("query2 time: " + str(end - start))
 
+start = time.time()
 top5DF = sq.sql(
     """
-    SELECT Name, SUM(Count) AS NUM FROM Births WHERE Year=""" + str(year) + """ GROUP BY Name ORDER BY NUM DESC LIMIT 3
+    SELECT Name, SUM(Count) AS NUM FROM Births WHERE Year=""" + str(year) + """ GROUP BY Name ORDER BY NUM DESC LIMIT 5
     """
 )
 top5DF.show()
+end = time.time()
+print("query3 time: " + str(end - start))
 
+start = time.time()
 Name = input("The name (like Mary, John): ")
 oneNameDF = sq.sql("SELECT Name, SUM(CAST(Count AS INT)) FROM Births WHERE Name='" + Name\
                    + "' GROUP BY Name")
 oneNameDF.show()
-# Saving DataFrame as JSON file
-# myDF.write.save("output", format="json")
-
-# Saving dataFrame as RDD and then text file
-# myDF.rdd.saveAsTextFile("/home/abhinav/Documents/mostPopularName")
-
-# refer official Spark documentation for further help eg. converting RDD to Dataframe or Dataframe to RDD, syntax, Spark Transformations and Actions etc.
+end = time.time()
+print("query3 time: " + str(end - start))
--- motor/sqlclean.py
+++ motor/sqlclean.py
@@ -3,6 +3,7 @@ from pyspark import SparkContext
 from pyspark.sql import SQLContext
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import time
 
 sc = SparkContext("local", "myapp")
 sq = SQLContext(sc)
@@ -34,15 +35,18 @@ rowDF.createOrReplaceTempView("Collisions")
 rowDF.printSchema()
 # rowDF.select("*").limit(3).show()
 # query1
+# start = time.time()
 # sq.sql("""
 #        SELECT KEY, PERSONS_INJURED, PERSONS_KILLED FROM Collisions
 #        """).write.csv(os.path.join("query1"), mode="overwrite")
-#
+# end = time.time()
+# print("query1: " + str(end - start))
+
 # query 2
+# start = time.time()
 # query2 = rowDF.select(year(rowDF["DATE"]).alias('Year'), \
 #              (rowDF["PERSONS_INJURED"] + rowDF["PERSONS_KILLED"]).alias("Incidents"))\
 #     .groupBy('Year').sum("Incidents")
-# print(type(query2))
 # query2.\
 #     repartition(1).\
 #     write.\
@@ -50,26 +54,33 @@ rowDF.printSchema()
 #     format("com.databricks.spark.csv").\
 #     option("header", "true").\
 #     save("query2.csv")
+# end = time.time()
+# print("query2: " + str(end - start))
 
 # query 3
-query3 = rowDF.select(year(rowDF["DATE"]).alias('Year'),\
-                      quarter(rowDF["DATE"]).alias('Quarter'),\
-             (rowDF["PERSONS_INJURED"] + rowDF["PERSONS_KILLED"]).alias("Incidents"))\
-    .groupBy('Year', 'Quarter').sum("Incidents")
-query3.\
-    repartition(1).\
-    write.\
-    mode("overwrite").\
-    format("com.databricks.spark.csv").\
-    option("header", "true").\
-    save("query3")
+# start = time.time()
+# query3 = rowDF.select(year(rowDF["DATE"]).alias('Year'),\
+#                       quarter(rowDF["DATE"]).alias('Quarter'),\
+#              (rowDF["PERSONS_INJURED"] + rowDF["PERSONS_KILLED"]).alias("Incidents"),\
+#                       )\
+#     .groupBy('Year', 'Quarter').sum("Incidents")
+# query3.\
+#     repartition(1).\
+#     write.\
+#     mode("overwrite").\
+#     format("com.databricks.spark.csv").\
+#     option("header", "true").\
+#     save("query3")
+# end = time.time()
+# print("query3: " + str(end - start))
 
 # query 4
+start = time.time()
 query4 = rowDF.select(year(rowDF["DATE"]).alias('Year'),\
                       rowDF["Borough"].alias('Borough'),\
                       month(rowDF["DATE"]).alias('Month'),\
              (rowDF["PERSONS_INJURED"] + rowDF["PERSONS_KILLED"]).alias("Incidents"))\
-    .groupBy('Borough', 'Year', 'Month').sum("Incidents")
+    .groupBy('Borough', 'Year', 'Month', 'Incidents').agg({"*": "count"})
 query4.\
     repartition(1).\
     write.\
@@ -77,3 +88,5 @@ query4.\
     format("com.databricks.spark.csv").\
     option("header", "true").\
     save("query4")
+end = time.time()
+print("query4: " + str(end - start))
